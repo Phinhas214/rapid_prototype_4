ab2a3ddf4604e2ccd704508ce92eb2e48372e3db
chaavan <surechaavanchidroop@gmail.com>
2025-12-01 22:40:02 -0800

    Main-Menu-Scene

---
+++ Assets/Scripts/ButtonHoverEffect.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("Hover Settings")]
+    [SerializeField] private float hoverScale = 1.1f;
+    [SerializeField] private float animationSpeed = 5f;
+    
+    private Vector3 originalScale;
+    private Vector3 targetScale;
+    private RectTransform rectTransform;
+    private bool isHovering = false;
+    
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        if (rectTransform != null)
+        {
+            originalScale = rectTransform.localScale;
+            targetScale = originalScale;
+        }
+    }
+    
+    void Update()
+    {
+        if (rectTransform != null)
+        {
+            // Smoothly animate to target scale
+            rectTransform.localScale = Vector3.Lerp(rectTransform.localScale, targetScale, Time.deltaTime * animationSpeed);
+        }
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovering = true;
+        targetScale = originalScale * hoverScale;
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovering = false;
+        targetScale = originalScale;
+    }
+}
+
